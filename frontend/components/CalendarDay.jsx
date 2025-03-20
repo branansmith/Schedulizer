9fc5610e1cf8a45b205a646f53bf70821047b4c3
branansmith <branansmith@gmail.com>
2025-03-20 17:46:55 -0400

    Created employee time component and added hsl

--- frontend/components/CalendarDay.jsx
+++ frontend/components/CalendarDay.jsx
@@ -1,10 +1,12 @@
-
+import EmployeeTime from './EmployeeTime';
 
 function CalendarDay() {
     return(
         <>
             <div className="calendar-day">
-                
+                <p className = "day-number">1</p>
+                <EmployeeTime/>
+                {/*potentially change colorway to grey and softer tones on the eyes, white is too bright*/}
             </div>
         </>
     )
---
+++ frontend/components/EmployeeTime.jsx
@@ -0,0 +1,15 @@
+
+
+function EmployeeTime() {
+    return (
+        <div className="employee-container">
+        <p className = "employee-name">Kamen</p>
+        <b className="employee-time">5:00PM - 9:00PM</b>
+        {/*figure out how to make the page repsonsive, so that when I shrink the page, it makes the entire calendar smaller*/}
+        </div>
+
+        
+    )
+}
+
+export default EmployeeTime;
